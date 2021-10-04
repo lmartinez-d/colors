e4b8411232237aaa30870c5c4a69608562533202
lmartinez-d <91565817+lmartinez-d@users.noreply.github.com>
2021-10-04 15:02:55 -0500

    Add files via upload

---
+++ circles_lizbeth.py
@@ -0,0 +1,12 @@
+#initialization
+import turtle
+import random
+lizbeth = turtle.Turtle
+
+#functions
+def randomDot():
+    t.color("purple")
+    t.begin_fill()
+    t.end_fill()
+    t.circle(40)
+#main
